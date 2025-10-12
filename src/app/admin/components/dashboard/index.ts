afa25a13d1ec4a456b72fc35085d7a74081bcfe8
mobin <you@example.com>
2025-10-12 15:59:15 +0300

    Add order details row component and integrate it into OrdersTable

--- src/app/admin/components/dashboard/index.ts
+++ src/app/admin/components/dashboard/index.ts
@@ -2,3 +2,4 @@ export * from "@/types/d.type"
 export { StatsCards } from './StatsCards'
 export { SalesChart } from './SalesChart'
 export { OrdersTable } from './OrdersTable'
+export { OrderDetailsRow } from './OrderDetailsRow';
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,13 +1,11 @@
-'use client'
+"use client";
 
-import {
-  OrdersTable,
-} from './components/dashboard'
-import { ShoppingCart } from 'lucide-react'
-import { useAuth } from '@/context/authContext';
-import { useQuery } from '@tanstack/react-query';
-import { getOrders } from '@/api/admin/adminSevices';
-import { ErrorHandler } from '../components/error-handler';
+import { OrdersTable } from "./components/dashboard";
+import { ShoppingCart } from "lucide-react";
+import { useAuth } from "@/context/authContext";
+import { useQuery } from "@tanstack/react-query";
+import { getOrders } from "@/api/admin/adminSevices";
+import { ErrorHandler } from "../components/error-handler";
 
 export default function AdminDashboardPage() {
   const { token } = useAuth();
@@ -15,16 +13,13 @@ export default function AdminDashboardPage() {
   const { data, isLoading, isError, isFetching } = useQuery({
     queryKey: ["Cart"],
     queryFn: () => getOrders(token ?? undefined),
-    enabled: !!token
+    enabled: !!token,
   });
 
 
   if (isError) {
-    return (
-      <ErrorHandler />
-    )
+    return <ErrorHandler />;
   }
-
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-50 via-gray-50 to-slate-100">
@@ -33,10 +28,16 @@ export default function AdminDashboardPage() {
           <div className="w-9 h-9 sm:w-10 sm:h-10 rounded-2xl bg-gradient-to-r from-slate-600 to-slate-700 flex items-center justify-center">
             <ShoppingCart className="w-4 h-4 sm:w-5 sm:h-5 text-white" />
           </div>
-          <h2 className="text-lg sm:text-2xl font-bold text-slate-800">جدول سفارش‌ها</h2>
+          <h2 className="text-lg sm:text-2xl font-bold text-slate-800">
+            جدول سفارش‌ها
+          </h2>
         </div>
-        <OrdersTable orders={data} isLoading={isLoading || isFetching} token={token ?? undefined}/>
+        <OrdersTable
+          orders={data}
+          isLoading={isLoading || isFetching}
+          token={token ?? undefined}
+        />
       </div>
     </div>
-  )
+  );
 }
